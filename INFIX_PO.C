62dc5003e6be1aa253f07205ed5fd419256c77dc
Ayyyman <53811550+Ayyyman@users.noreply.github.com>
2019-08-27 12:07:58 +0530

    Update INFIX_PO.C

--- INFIX_PO.C
+++ INFIX_PO.C
@@ -1,68 +1,95 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <string.h>
 #include<ctype.h>
+#define SIZE 100
+int top=-1;
+char s[SIZE];
 
-char stack[20];
-int top = -1;
-
-void push(char x)
+void push(char val)
 {
-	stack[++top]=x;
+    if(top==SIZE-1)
+        printf("stack full");
+    else{
+        top=top+1;
+        s[top]=val;
+    }
 }
 
 char pop()
 {
-	if(top == -1)
-		return -1;
-	else
-		return stack[top--];
+    char val=' ';
+    if(top==-1)
+        printf("stack empty");
+    else{
+        val=s[top];
+        top=top-1;
+    }
+    return val;
 }
 
-int priority(char x)
+int priority(char op)
 {
-	if(x == '(')
-		return 0;
-	if(x=='+' || x=='-')
-		return 1;
-	if(x == '*' || x == '/')
-		return 2;
+    if(op=='%' || op=='/' || op=='*' )
+        return 1;
+    else
+        return 0;
 }
 
-
-
-void main()
+void infixToPostfix(char in[],char post[])
 {
-	char expre[20];
-	char *e,x;
-        clrscr();
-	printf("Enter The Expression To Be Converted : \n");
-	scanf("%s", expre);
-	e = expre;
-	
-	while(*e != '\0')
-	{
-		if(isalnum(*e))
-			printf("%c",*e);
-		else if(*e == '(')
-			push(*e);
-			
-		else if(*e == ')')
-		{
-			while((x == pop()) != '(')
-				printf("%c" ,x);
-		}
-		else
-		{
-			while((priority(stack[top]))>=priority(*e))
-				printf("%c", pop());
-				push(*e);
-		}
-		e++;
-	}
-	while(top!=-1)
-	{
-		printf("%c ", pop());
-	}
-        getch();
+    int i=0,j=0;
+    while(in[i]!='\0')
+    {
+        if(in[i]=='(')
+        {
+             push(in[i]);
+             i++;
+        }
+        else if(in[i]==')')
+        {
+            while(top!=-1 && s[top]!='(')
+            {
+                post[j]=pop();
+                j++;
+            }
+            pop();
+            i++;
+        }
+        else if(isdigit(in[i]) || isalpha(in[i]))
+        {
+            post[j]=in[i];
+            i++;
+            j++;
+        }
+        else if(in[i]=='%' || in[i]=='/' || in[i]=='-' || in[i]=='*' || in[i]=='+' )
+        {
+            while(top!=-1 && s[top]!='(' && (priority(s[top])>=priority(in[i])))
+            {
+                post[j]=pop();
+                j++;
+            }
+            push(in[i]);
+            i++;
+        }
+        else
+            printf("invalid expression\n");
+    }
+    while(top!=-1 && s[top]!='(')
+    {
+        post[j]=pop();
+        j++;
+    }
+    post[j]='\0';
+}
 
+int main()
+{
+    char infix[100],postfix[100];
+    printf("enter infix expression: ");
+    scanf("%s",infix);
+    strcpy(postfix,"");
+    infixToPostfix(infix,postfix);
+    printf("postfix: ");
+    printf("%s",postfix);
+    return 0;
 }
