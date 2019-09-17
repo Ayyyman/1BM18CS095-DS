22108c0276182b657209b91597deaa7e15f65027
Ayyyman <53811550+Ayyyman@users.noreply.github.com>
2019-09-17 12:34:03 +0530

    lab pgm 5 uploaded

---
+++ linked.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{ 
+	int data;
+	struct node*next;
+};
+typedef struct node*NODE;
+
+NODE getnode()
+{
+	NODE p;
+	p=(NODE)malloc(sizeof(struct node));
+	if(p!=NULL)
+	  return p;
+	else
+	{ 
+	  printf("No memory allocation\n");
+	  exit(0);
+	}
+}
+
+NODE insert_front(NODE head,int item)
+{
+	NODE p;
+	p=getnode();
+	p->data=item;
+	p->next=head;
+	head=p;
+	return head;
+}
+
+void display(NODE head)
+{
+	NODE p;
+	if(head==NULL)
+	{
+	 printf("List is empty\n");
+	}
+	p=head;
+	while(p!=NULL)
+	{
+	 printf("%d\n",p->data);
+	 p=p->next;
+	}
+}
+
+NODE insert_end(NODE head,int item)
+{
+	NODE p,q;
+	q=getnode();
+	q->data=item;
+	q->next=NULL;
+	if(head==NULL)
+	 {
+	  return q;
+	 }
+	p=head;
+	while(p->next!=NULL)
+	 p=p->next;
+	 p->next=q;
+	 return head;
+}
+
+void main()
+{
+	NODE head=NULL;
+	int option,ele,value;
+	while(1)
+	{
+ 	 printf("\n------MENU------\n");
+	 printf("\n1)Insert Front\n2)Insert End\n3)Display\n4)Exit\n");
+	 printf("Enter option\n");
+	 scanf("%d",&option);
+	 switch(option)
+	  {
+		case 1: printf("Enter element to be inserted\n");
+			scanf("%d",&ele);
+			head=insert_front(head,ele);
+			break;
+		
+		case 2: printf("Enter the element to be inserted\n");
+			scanf("%d",&ele);
+			head=insert_end(head,ele);
+			break;
+		
+		case 3: printf("Elements in list are\n");
+			display(head);
+			break;
+
+		case 4: exit(0);
+			break;
+
+		default: printf("Invalid choice");
+	  }
+	}
+}
+
+
